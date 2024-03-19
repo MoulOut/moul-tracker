e741647566f65158ee3f2f6e189618089549e04d
MoulOut <mmoulinlima@outlook.com>
2024-03-19 11:11:14 -0300

    feat: :sparkles: add: showing project on task list

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -1,10 +1,13 @@
 <template>
   <Box>
     <div class="columns">
-      <div class="column is-7">
+      <div class="column is-4">
         {{ tarefa.descricao || 'Tarefa sem descrição' }}
       </div>
-      <div class="column">
+      <div class="column is-4">
+        {{ tarefa.projeto.nome }}
+      </div>
+      <div class="column is-1">
         <Cronometro :tempo-em-segundos="tarefa.duracaoEmSegundos" />
       </div>
     </div>
