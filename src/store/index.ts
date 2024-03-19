7a4bf26365331f08b0b0a21f756bff40c5a307ba
MoulOut <mmoulinlima@outlook.com>
2024-03-19 10:35:17 -0300

    feat: :sparkles: add: types for notifications

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,9 +6,11 @@ import {
   ALTERA_PROJETO,
   EXCLUIR_PROJETO,
 } from './tipo-mutations';
+import { INotificacao, TipoNotificacao } from '@/interface/INotificacao';
 
 interface Estado {
   projetos: IProjeto[];
+  notificacoes: INotificacao[];
 }
 
 export const storeKey: InjectionKey<Store<Estado>> = Symbol();
@@ -16,6 +18,26 @@ export const storeKey: InjectionKey<Store<Estado>> = Symbol();
 export const store = createStore<Estado>({
   state: {
     projetos: [],
+    notificacoes: [
+      {
+        id: 1,
+        texto: 'Uma notificacao de sucesso',
+        titulo: 'Sucesso',
+        tipo: TipoNotificacao.SUCESSO,
+      },
+      {
+        id: 2,
+        texto: 'Uma notificacao de atenção',
+        titulo: 'Atenção',
+        tipo: TipoNotificacao.ATENCAO,
+      },
+      {
+        id: 3,
+        texto: 'Uma notificacao de falha',
+        titulo: 'Falha',
+        tipo: TipoNotificacao.FALHA,
+      },
+    ],
   },
   mutations: {
     [ADICIONA_PROJETO](state, nomeDoProjeto: string) {
